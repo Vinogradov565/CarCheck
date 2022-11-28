6cbe1f994133a9e7d5f5d8f5bf0cd5a36fb0873b
Vinogradov565 <116278453+Vinogradov565@users.noreply.github.com>
2022-11-28 19:22:53 +0300

    Add files via upload

---
+++ CarCheck/src/Car.java
@@ -0,0 +1,79 @@
+public class Car {
+    public String model;
+    public int age;
+    private boolean wasInAccident = false; // true
+
+    public Car(String model, int age) {
+        this.model = model;
+        this.age = age;
+    }
+
+    public Car(){
+
+    }
+
+    public boolean isWasInAccident() {
+        return wasInAccident;
+    }
+
+    public void setWasInAccident(boolean wasInAccident) {
+        this.wasInAccident = wasInAccident;
+    }
+
+    public static void checkCar(){
+        Car car = new Car("BMW M5 F90", 4);
+        System.out.println("Автомобиль: " + car.model);
+        System.out.println("Возраст: " + car.age);
+
+        if (car.isWasInAccident() == true){
+            System.out.println("Машина нуждается в ремонте и её состояние неудовлетворительно");
+        } else {
+            if (car.age < 3){
+                int fromNumber = 100;
+                int toNumber = 20000;
+
+                int carMileage = fromNumber + (int) (Math.random() * toNumber);
+
+                System.out.println("Проверка пробега: " + carMileage + " км");
+
+                if (carMileage >= 10000){
+                    System.out.println("Состояние автомобиля хорошее, требуется замена масла.");
+                } else {
+                    System.out.println("Состояние автомобился идеальное.");
+                }
+
+            } else if (car.age >= 3 && car.age <= 6){
+                int fromFirstNumber = 10000;
+                int toFirstNumber = 60000;
+
+                int carMileage = fromFirstNumber + (int) (Math.random() * toFirstNumber);
+
+                int fromSecondNumber = 0;
+                int toSecondNumber = 1;
+
+                double engineWear = fromSecondNumber + (Math.random() * toSecondNumber);
+
+                System.out.println("Проверка пробега: " + carMileage + " км");
+                System.out.print("Проверка износа двигателя: ");
+                System.out.printf("%.2f", engineWear);
+                System.out.println();
+
+                if (engineWear == 0){
+                    System.out.println("Двигатель без износа, требуется замена масла.");
+                } else if (engineWear >= 0.5 && engineWear <= 1) {
+                    System.out.println("Двигатель износился и нуждается в ремонте.");
+                }
+            } else if (car.age > 6){
+                int carMileage = 60000;
+                double engineWear = 1;
+
+                System.out.println("Проверка пробега: " + carMileage + " км");
+                System.out.print("Проверка износа двигателя: ");
+                System.out.printf("%.2f", engineWear);
+                System.out.println();
+
+                System.out.println("Машина нуждается в ремонте.");
+            }
+        }
+    }
+}
